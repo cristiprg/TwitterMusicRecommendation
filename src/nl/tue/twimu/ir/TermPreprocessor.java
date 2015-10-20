94ed39b29c0a287a99f9e78865b5864f26876884
AKaasJ <Andreasjoh91@gmail.com>
2015-10-20 21:11:28 +0200

    Preprocessing now includes hashtags (doesn't trim them) and uses Porter Stemming.

--- src/nl/tue/twimu/ir/TermPreprocessor.java
+++ src/nl/tue/twimu/ir/TermPreprocessor.java
@@ -3,24 +3,39 @@ package nl.tue.twimu.ir;
 import nl.tue.twimu.ir.util.Stemmer;
 
 public class TermPreprocessor {
+	
+	public static boolean doStemming = true;
+	public static boolean trimHashTags = false;
 
 	public static String termPreProcessing(String term) {
 
 		// to lower case
 		term = term.toLowerCase();
 
-		// remove trailing and leading non-aplhanumeric
-		// TODO: except the # character? to retain hashtags -
-		// (^[^a-z0-9#:]*|[^a-z0-9]*$)
-		term = term.replaceAll("(^[^a-z0-9]*|[^a-z0-9]*$)", "");
+		String regEx = null;
+		
+		if (trimHashTags == true)
+			regEx = "(^[^a-z0-9]*|[^a-z0-9]*$)";
+		else
+			regEx = "(^[^a-z0-9#:]*|[^a-z0-9]*$)";
+		
+		
+		//Do stemming
+		if (doStemming == true){
+			//use regEx to remove e.g. exclamation marks, but keep hashtags at the end or beginning of words
+			term = term.replaceAll(regEx, "");
+		
+			// perform stemming.
+			Stemmer stemmer = new Stemmer();
+			if (term.length() > 0) {
+				// w = porterAnalyzer.stemQuery(w);
+				stemmer.add(term.toCharArray(), term.toCharArray().length);
+				stemmer.stem();
+				term = stemmer.toString();
+				}
+		
 
-		// perform stemming on non-hashtags
-		Stemmer stemmer = new Stemmer();
-		if (term.length() > 0 && term.getBytes()[0] != '#') {
-			// w = porterAnalyzer.stemQuery(w);
-			stemmer.add(term.toCharArray(), term.toCharArray().length);
-			stemmer.stem();
-			term = stemmer.toString();
+
 		}
 
 		return term;
